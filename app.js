6241b659863588d0f7d9789b20893b033ab2e769
Robin Bolscher <robinbolscher@gmail.com>
2015-09-20 00:09:21 +0200

    Started re-built to use different lifx js library
    Started rebuilding the existing app the use node-licx which is more
    recent and supported than the old one. Validated app for App Store.

--- app.js
+++ app.js
@@ -1,70 +1,7 @@
 "use strict";
-  
-function App() 
-{
-	
-}
 
-module.exports = App;
+var self = module.exports = {
+    init: function () {
 
-App.prototype.init = function(){
-
-	// flow:action:on
-	Homey.manager('flow').on('action.on', function( args, callback ){
-		if( typeof args.device == 'undefined' ) return;
-		var driver = Homey.manager('drivers').getDriver( args.device.driver.id );
-
-		//TODO check for specific bulb or all bulbs
-		// Turn on bulb(s)
-		driver.capabilities.onoff.set( args.device.data, true, args.device.bulb, callback );
-
-		//TODO integrate color object
-		// Set given color on bulb(s)
-		if(args.hue){
-			driver.capabilities.hue.set( args.device.data, args.hue, callback )
-		}
-		if(args.saturation){
-			driver.capabilities.saturation.set( args.device.data, args.saturation, callback )
-		}
-		if(args.brightness){
-			driver.capabilities.brightness.set( args.device.data, args.brightness, callback )
-		}
-		if(args.temperature){
-			driver.capabilities.temperature.set( args.device.data, args.temperature, callback )
-		}
-	});
-
-	// flow:action:off
-	Homey.manager('flow').on('action.off', function( args, callback ){
-		if( typeof args.device == 'undefined' ) return;
-		var driver = Homey.manager('drivers').getDriver( args.device.driver.id );
-
-		// Turn off bulb(s)
-		driver.capabilities.onoff.set( args.device.data, false, args.device.bulb, callback );
-	});
-
-	// flow:action:color
-	Homey.manager('flow').on('action.color', function( args, callback ){
-		if( typeof args.device == 'undefined' ) return;
-		var driver = Homey.manager('drivers').getDriver( args.device.driver.id );
-
-		//TODO integrate color object
-		// Set given color on bulb(s)
-		if(args.hue){
-			driver.capabilities.hue.set( args.device.data, args.hue, callback )
-		}
-		if(args.saturation){
-			driver.capabilities.saturation.set( args.device.data, args.saturation, callback )
-		}
-		if(args.brightness){
-			driver.capabilities.brightness.set( args.device.data, args.brightness, callback )
-		}
-		if(args.temperature){
-			driver.capabilities.temperature.set( args.device.data, args.temperature, callback )
-		}
-	});
+    }
 };
-
-App.prototype.speech = function( speech ) {
-	Homey.manager('speech-output').say( "hello" );
-}
--- drivers/lifx/driver.js
+++ drivers/lifx/driver.js
@@ -1,235 +1,164 @@
-var lifx = require('lifx');
-var lx;
+var Lifx = require( 'node-lifx' ).Client;
+var client = new Lifx();
 
 /**
- * Holds all found bulb objects
- * @type {Array}
+ * Global settings object, to keep track of color settings for multiple bulbs
  */
-var bulbs = [];
+var globalSettings = {
+    duration: 500
+};
+
+/***
+ * Initially start Lifx client to search for bulbs on the network
+ * @param devices (already installed)
+ * @param callback
+ */
+module.exports.init = function ( devices, callback ) {
+
+    // Initialize new Lifx client
+    client.init();
+
+    // Listing for incoming bulbs
+    client.on( 'bulb-new', function ( bulb ) {
+        console.log( 'New bulb found: ' + bulb.address + ':' + bulb.port );
+    } );
+
+    callback (true);
+};
 
 /**
- * Function that finds a bulb using its device_id
- * @param device_id
+ * Pairing process that calls list_devices when in need of all devices
+ * Lifx can find, here the devices array is built and send to the front-end
+ */
+module.exports.pair = {
+
+    /**
+     * Constructs array of all available devices
+     */
+    list_devices: function ( callback ) {
+        var devices = [];
+
+        // Loop bulbs found by Lifx
+        client.lights().forEach( function ( bulb ) {
+
+            // Add them to create devices array
+            devices.push( {
+                data: {
+                    id: bulb.id, //TODO check
+                    bulb: bulb
+                },
+                name: bulb.label //TODO check
+            } );
+        } );
+
+        // Send devices array to front-end
+        callback( devices );
+    }
+};
+
+/**
+ * These represent the capabilities of the Lifx light bulbs
+ */
+module.exports.capabilities = {
+
+    onoff: {
+        get: function ( device_data, callback ) {
+            if ( device_data instanceof Error ) return callback( device_data );
+
+            // Return current bulb state
+            if ( callback ) callback( device_data.bulb.state ); //TODO check
+        },
+        set: function ( device_data, onoff, callback ) {
+
+            if ( onoff ) {
+
+                // Turn bulb on with global duration setting
+                device_data.bulb.on( globalSettings.duration );
+
+                // Let know that bulb is turned on
+                callback( true );
+            }
+            else {
+
+                // Turn bulb off with global duration setting
+                device_data.bulb.off( globalSettings.duration );
+
+                // Let know that bulb is turned off
+                callback( false );
+            }
+        }
+    },
+    hue: {
+        get: function ( device_data, callback ) {
+            if ( device_data instanceof Error ) return callback( device_data );
+
+            // Return mapped hue
+            if ( callback ) callback( mapRange( device_data.bulb.hue, 0, 360, 0, 100 ) ); //TODO check
+        },
+        set: function ( device_data, hue, callback ) {
+
+            device_data.bulb.color( mapRange( hue, 0, 100, 0, 360 ), device_data.bulb.saturation, device_data.bulb.brightness );
+
+            if ( callback ) callback( hue );
+        }
+    },
+
+    saturation: {
+        get: function ( device_data, callback ) {
+            if ( device_data instanceof Error ) return callback( device_data );
+
+            // Return saturation
+            if ( callback ) callback( device_data.bulb.saturation ); //TODO check
+        },
+        set: function ( device_data, saturation, callback ) {
+
+            device_data.bulb.color( device_data.bulb.hue, saturation, device_data.bulb.brightness );
+
+            if ( callback ) callback( saturation );
+        }
+    },
+
+    brightness: {
+        get: function ( device_data, callback ) {
+            if ( device_data instanceof Error ) return callback( device_data );
+
+            // Return brightness
+            if ( callback ) callback( device_data.bulb.brightness ); //TODO check
+        },
+        set: function ( device_data, brightness, callback ) {
+
+            device_data.bulb.color( device_data.bulb.hue, device_data.bulb.saturation, brightness );
+
+            if ( callback ) callback( brightness );
+        }
+    },
+
+    temperature: {
+        get: function ( device_data, callback ) {
+            if ( device_data instanceof Error ) return callback( device_data );
+
+            // Return mapped kelvin value
+            if ( callback ) callback( mapRange( device_data.bulb.kelvin, 2500, 9000, 0, 100 ) ); //TODO check
+        },
+        set: function ( device_data, temperature, callback ) {
+
+            // Convert temperature to usable range for Lifx and update temperature
+            device_data.bulb.color( device_data.bulb.hue, device_data.bulb.saturation, device_data.bulb.brightness, mapRange( temperature, 0, 100, 2500, 9000 ) ); //TODO check
+
+            if ( callback ) callback( temperature );
+        }
+    }
+};
+
+/**
+ * Util function that maps values between ranges
+ * @param value
+ * @param low1
+ * @param high1
+ * @param low2
+ * @param high2
  * @returns {*}
  */
-var getBulb = function (device_id) {
-
-	// If no device id, return
-	if(device_id === null) return;
-
-	// Loop over bulb objects
-	for (var x = 0; x < bulbs.length; x++) {
-
-		// Return if one found with matching ids
-		if (bulbs[x].id === device_id) {
-			return bulbs[x];
-		}
-	}
-};
-
-/**
- * Function that updates a single light bulb, or all in bulk,
- * depending on the device id provided.
- * @param key hue/saturation/luminance/whiteColour/timing
- * @param value 16bit updated value
- * @param id device_data.id
- */
-var updateLightSettings = function (key, value, id) {
-	var bulb = getBulb(id);
-
-	// Update bulb color settings
-	if (bulb) {
-
-		// Store updated value in bulb object
-		bulb[key] = value;
-
-		// Broadcast new color settings to bulb
-		lx.lightsColour(intTo16bit(bulb.hue), intTo16bit(bulb.saturation), intTo16bit(bulb.luminance), intTo16bit(bulb.whiteColour), bulb.timing, bulb);
-
-	} // Update global color settings
-	else {
-
-		// Store updated value in global color settings
-		globalColorSettings[key] = value;
-
-		// Broadcast new color settings to all bulbs
-		lx.lightsColour(intTo16bit(globalColorSettings.hue), intTo16bit(globalColorSettings.saturation), intTo16bit(globalColorSettings.luminance), intTo16bit(globalColorSettings.whiteColour), globalColorSettings.timing);
-	}
+function mapRange ( value, low1, high1, low2, high2 ) {
+    return low2 + (high2 - low2) * (value - low1) / (high1 - low1);
 }
-
-/**
- * Function that converts 0 - 65535 to 0x0000 - 0xffff
- * @param i
- * @returns {string}
- */
-var intTo16bit = function( i ) {
-	i = '0x' + i.toString(16);
-	return i;
-};
-
-/**
- * Global color settings object, to keep track of color settings for multiple bulbs
- */
-var globalColorSettings = {
-	hue: 0,
-	saturation: 0,
-	luminance: 8,
-	whiteColour: 0,
-	timing: 500
-};
-
-module.exports.init = function ( devices, callback ) {
-	// Start looking for lifx bulbs
-	lx = lifx.init();
-
-	// Found a bulb
-	lx.on('bulb', function(b) {
-
-		// Add new bulb object to bulbs
-		bulbs.push({
-			name: b.name,
-			ip: b.addr,
-			get id() {
-				return this.name + '_' + this.ip
-			},
-			hue: 0x0000,
-			saturation: 0x0000,
-			luminance: 0x8000,
-			whiteColour: 0x0af0,
-			timing: 500,
-			onoff: true
-		});
-	});
-
-	if(callback) callback();
-}
-
-module.exports.pair = {
-	list_devices: function ( callback, emit, data){
-		//TODO implement listing devices
-		callback (bulbs);
-	},
-	add_device: function ( ) {
-		//TODO implement add device
-	}
-}
-
-module.exports.capabilities = {
-	name: {
-		get: function (device_data, callback) {
-			if (callback) callback();
-
-			//TODO access device name here
-			return device_data.name;
-
-		},
-		set: function (device_data, name, callback) {
-
-			//TODO set device name here
-			device_data.name = name;
-
-			if (callback) callback();
-		}
-	},
-
-	onoff: {
-		get: function (device_data, callback) {
-			if (callback) callback();
-
-			var bulb = getBulb(device_data);
-
-			return bulb.onoff;
-		},
-		set: function (device_data, onoff, callback) {
-			var bulb = getBulb(device_data.id);
-
-			if(onoff){
-				if (bulb) {
-					lx.lightsOn(bulb);
-				}
-				else {
-					lx.lightsOn();
-				}
-			}
-			else {
-				if (bulb) {
-					lx.lightsOff(bulb);
-				}
-				else {
-					lx.lightsOff();
-				}
-			}
-
-			if (callback) callback();
-		}
-	},
-
-	//TODO find out values to set (0-100 or different)
-	hue: {
-		get: function (device_data, callback) {
-			if (callback) callback();
-
-			var bulb = getBulb(device_data);
-
-			return bulb.hue;
-		},
-		set: function (device_data, hue, callback) {
-
-			//Update hue of lights
-			updateLightSettings("hue", hue,  device_data.id || null);
-
-			if (callback) callback();
-		}
-	},
-
-	saturation: {
-		get: function (device_data, callback) {
-			if (callback) callback();
-
-			var bulb = getBulb(device_data);
-
-			return bulb.saturation;
-		},
-		set: function (device_data, saturation, callback) {
-
-			//Update saturation of lights
-			updateLightSettings("saturation", saturation,  device_data.id || null);
-
-			if (callback) callback();
-		}
-	},
-
-	temperature: {
-		get: function (device_data, callback) {
-			if (callback) callback();
-
-			var bulb = getBulb(device_data);
-
-			return bulb.onoff;
-		},
-		set: function (device_data, temperature, callback) {
-
-			//Update temperature of lights
-			updateLightSettings("whiteColour", temperature, device_data.id || null);
-
-			if (callback) callback();
-		}
-	},
-
-	brightness: {
-		get: function (device_data, callback) {
-			if (callback) callback();
-
-			var bulb = getBulb(device_data);
-
-			return bulb.onoff;
-		},
-		set: function (device_data, luminance, callback) {
-
-			//Update luminance of lights
-			updateLightSettings("luminance", luminance, device_data.id || null);
-
-			if (callback) callback();
-		}
-	}
-};
